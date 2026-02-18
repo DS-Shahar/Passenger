fed15c6b385666bf9f7fd6f9d8576366650bffd3
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2026-02-18 13:42:09 +0200

    Add Main class with flight assignment methods

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    // סעיף א:
+    public static void assignFlight(Flight[] flights, Passenger p) {
+
+    }
+
+    // סעיף ב:
+    public static void cancelAndReplace(Flight[] flights, int i, Passenger canceling) {
+      
+    }
+
+    // סעיף ג:
+    /*
+     לכתוב מה הסיבוכיות
+    */
+}
